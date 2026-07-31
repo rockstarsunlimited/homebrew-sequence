ae3f2dfd2a5c4752653b1ed1ec19c5092bdaba03
James Seddon <js@rockstarsunlimited.co>
2026-08-01 00:33:50 +0100

    Release Sequence CLI 1.0.4

--- Formula/sequence.rb
+++ Formula/sequence.rb
@@ -5,9 +5,9 @@
 class Sequence < Formula
   desc "Command-line interface for Sequence media workflows"
   homepage "https://sequencetool.com/docs/cli"
-  url "https://github.com/rockstarsunlimited/sequencetool/releases/download/cli-v1.0.3/sq-macos-arm64.zip"
-  version "1.0.3"
-  sha256 "ad7a0e1cfe7746ae4993a6037ccd2cc48ee2c43f65aefbeb2f45641bd8d9424b"
+  url "https://github.com/rockstarsunlimited/sequencetool/releases/download/cli-v1.0.4/sq-macos-arm64.zip"
+  version "1.0.4"
+  sha256 "b244d6c5a7d5fd567fbbfc825dc9f5eb164037a29822e1998f05ae02f5404c47"
   license "LicenseRef-Sequence-Proprietary"
 
   depends_on arch: :arm64
@@ -21,7 +21,7 @@ class Sequence < Formula
   end
 
   test do
-    assert_match "sq 1.0.3", shell_output("#{bin}/sq --version")
-    assert_match "action catalog ABI: v1", shell_output("#{bin}/sq doctor")
+    assert_match "sq 1.0.4", shell_output("#{bin}/sq --version")
+    assert_match "project.list", shell_output("#{bin}/sq actions list")
   end
 end
